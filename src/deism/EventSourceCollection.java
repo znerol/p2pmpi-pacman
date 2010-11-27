15d4814d160d167b80903585815a73c3cd97af54
Lorenz Schori <lo@znerol.ch>
2010-11-27 18:59:44 +0100

    Rewrite EventSourceCollection.receive in order to support duplicate events

--- src/deism/EventSourceCollection.java
+++ src/deism/EventSourceCollection.java
@@ -1,9 +1,8 @@
 package deism;
 
 import java.util.Arrays;
-import java.util.Iterator;
-import java.util.SortedMap;
-import java.util.TreeMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Special EventSource aggregating the events from multiple EventSources
@@ -22,29 +21,36 @@ public class EventSourceCollection implements EventSource {
     
     @Override
     public Event receive(long currentSimtime) {
-        SortedMap<Event, EventSource> eventsAndSources =
-            new TreeMap<Event, EventSource>();
-        
-        currentSource = null;
-        
-        for (EventSource source : eventSources) {
-            Event event = source.receive(currentSimtime);
-            if (event != null) {
-                eventsAndSources.put(event, source);
-            }
-        }
-
+        Map<EventSource, Event> candidates =
+            new LinkedHashMap<EventSource, Event>();
         Event result = null;
-        Iterator<Event> it = eventsAndSources.keySet().iterator();
-        if (it.hasNext()) {
-            result = it.next();
-            currentSource = eventsAndSources.get(result);
-            while (it.hasNext()) {
-                Event event = it.next();
-                eventsAndSources.get(event).reject(event);
+
+        // poll all event sources and knock up the candidates map.
+        for (EventSource source : eventSources) {
+            Event candidateEvent = source.receive(currentSimtime);
+            if (candidateEvent != null) {
+                candidates.put(source, candidateEvent);
             }
         }
         
+        // find the event with the least timestamp
+        currentSource = null;
+        for (EventSource source : candidates.keySet()) {
+            Event candidateEvent = candidates.get(source);
+            if (result == null || candidateEvent.compareTo(result) < 0) {
+                result = candidateEvent;
+                currentSource = source;
+            }
+        }
+        
+        // reject the events from all the other sources
+        if (currentSource != null) {
+            candidates.remove(currentSource);
+            for (EventSource source : candidates.keySet()) {
+                source.reject(candidates.get(source));
+            }
+        }
+                
         return result;
     }
 
