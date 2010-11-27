e14110e711785cebe2c8a597dc76c6fb0ec8ab4a
Lorenz Schori <lo@znerol.ch>
2010-11-27 19:00:28 +0100

    EventDispatcherCollection analog to EventSourceCollection

---
+++ src/deism/EventDispatcherCollection.java
@@ -0,0 +1,22 @@
+package deism;
+
+import java.util.Arrays;
+
+public class EventDispatcherCollection implements EventDispatcher {
+    private final Iterable<EventDispatcher> dispatchers;
+    
+    public EventDispatcherCollection(Iterable<EventDispatcher> dispatchers) {
+        this.dispatchers = dispatchers;
+    }
+    
+    public EventDispatcherCollection(EventDispatcher[] dispatchers) {
+        this(Arrays.asList(dispatchers));
+    }
+    
+    @Override
+    public void dispatchEvent(Event event) {
+        for (EventDispatcher dispatcher : dispatchers) {
+            dispatcher.dispatchEvent(event);
+        }
+    }
+}
