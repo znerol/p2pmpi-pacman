e17b3d14e58c7d8d2ed8cb745d7f2c65a24ce498
Lorenz Schori <lo@znerol.ch>
2010-11-30 21:50:15 +0100

    MPI receive loop in second thread in MpiEventSource

--- src/p2pmpi/MpiEventSource.java
+++ src/p2pmpi/MpiEventSource.java
@@ -1,45 +1,85 @@
 package p2pmpi;
 
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
+
 import p2pmpi.mpi.IntraComm;
 import p2pmpi.mpi.MPI;
 
 import deism.Event;
 import deism.EventSource;
+import deism.ExecutionGovernor;
 
 public class MpiEventSource implements EventSource {
 
     private final int mpisender;
-    private final int mpireceiver;
     private final int mpitag;
     private final IntraComm mpicomm;
-    private Event currentEvent;
+    private final Queue<Event> events = new PriorityBlockingQueue<Event>();
+    private final ExecutionGovernor governor;
+    private final Worker worker;
 
     public MpiEventSource(IntraComm comm, int mpisender, int mpireceiver,
-            int mpitag) {
+            int mpitag, ExecutionGovernor governor) {
         this.mpicomm = comm;
         this.mpisender = mpisender;
-        this.mpireceiver = mpireceiver;
         this.mpitag = mpitag;
+        this.governor = governor;
+
+        if (mpicomm.Rank() == mpireceiver) {
+            worker = new Worker();
+        }
+        else {
+            worker = null;
+        }
+    }
+
+    @Override
+    public void start(long startSimtime) {
+        if (worker != null) {
+            worker.start();
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (worker != null) {
+            worker.terminate();
+        }
     }
 
     @Override
     public Event receive(long currentSimtime) {
-        if (currentEvent == null && mpicomm.Rank() == mpireceiver) {
-            Event[] buffer = new Event[1];
-            mpicomm.Irecv(buffer, 0, 1, MPI.OBJECT, mpisender, mpitag);
-            currentEvent = buffer[0];
-        }
-
-        return currentEvent;
+        return events.peek();
     }
 
     @Override
     public void accept(Event event) {
-        assert (event == currentEvent);
-        currentEvent = null;
+        events.remove(event);
     }
 
     @Override
     public void reject(Event event) {
+    }
+
+    private class Worker extends Thread {
+        private boolean done = false;
+
+        @Override
+        public void run() {
+            while (!done) {
+                Event[] recvBuffer = { null };
+                mpicomm.Recv(recvBuffer, 0, 1, MPI.OBJECT, mpisender, mpitag);
+                events.offer(recvBuffer[0]);
+                governor.resume(events.peek().getSimtime());
+            }
+        }
+
+        public void terminate() {
+            done = true;
+            if (isAlive()) {
+                interrupt();
+            }
+        }
     }
 }
