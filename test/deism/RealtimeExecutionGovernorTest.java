756a71cc11a6d480c676ed0864068c8f43ae447c
Lorenz Schori <lo@znerol.ch>
2010-12-03 00:00:05 +0100

    RealtimeExecutionGovernorTest. A hard one, mocked and threaded

---
+++ test/deism/RealtimeExecutionGovernorTest.java
@@ -0,0 +1,168 @@
+package deism;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.RunnableFuture;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RealtimeExecutionGovernorTest {
+    @Mock Clock clock;
+    RealtimeExecutionGovernor governor;
+    long wakeupTime;
+
+    @Before
+    public void setUp() {
+        governor = new RealtimeExecutionGovernor(clock);
+    }
+
+    public void busyWaitForThreadState(Thread thread, Thread.State state)
+            throws InterruptedException {
+        int i = 0;
+        while(thread.getState() != state) {
+            if (i++ < 10) {
+                Thread.sleep(10);
+            }
+            else {
+                fail("Governor must suspend execution of its thread.");
+            }
+        }
+    }
+
+    @Test
+    public void testSuspendResume() throws Exception {
+        // define a timestamp returned by clock and therefore by governor
+        // suspend when wakeup is called
+        when(clock.getSimtime()).thenReturn(42L);
+
+        // setup a future task wrapping governor.suspend
+        RunnableFuture<Long> suspender = new FutureTask<Long>(
+                new Callable<Long>() {
+                    @Override
+                    public Long call() throws Exception {
+                        return governor.suspend();
+                    }
+                });
+        Thread thread = new Thread(suspender);
+        thread.start();
+
+        // wait until suspender got called and governor suspended the thread
+        busyWaitForThreadState(thread, Thread.State.WAITING);
+
+        // resume governor. This method is called when an EventSource running
+        // in its own thread wants to get polled.
+        governor.resume();
+
+        // verify the result and the call to clock
+        long result = suspender.get();
+        assertEquals(42L, result);
+
+        verify(clock).getSimtime();
+    }
+
+    @Test
+    public void testSuspendUntilButContinueImmediately() throws Exception {
+        // define a timestamp returned by clock and therefore by governor
+        // suspend when wakeup is called
+        when(clock.getRealtime()).thenReturn(10L);
+        when(clock.getRealtime(10L)).thenReturn(10L);
+
+        // we don't expect governor.suspend(10L) to suspend (wait) at all
+        // because the delay (clock.getRealtime(10L) - clock.getRealtime())
+        // should be zero.
+        long result = governor.suspendUntil(10L);
+        assertEquals(10L, result);
+
+        verify(clock).getRealtime(10L);
+        verify(clock).getRealtime();
+    }
+
+    @Test
+    public void testSuspendUntilReturnAtTime() throws Exception {
+        // define a timestamp returned by clock and therefore by governor
+        // suspend when wakeup is called
+        when(clock.getRealtime()).thenReturn(0L);
+        when(clock.getRealtime(10L)).thenReturn(10L);
+
+        // we expect that the junit thread is delayed for 10 milliseconds.
+        long result = governor.suspendUntil(10L);
+        assertEquals(10L, result);
+
+        verify(clock).getRealtime(10L);
+        verify(clock).getRealtime();
+    }
+
+    @Test
+    public void testSuspendUntilResumeEarly() throws Exception {
+        // define a timestamp returned by clock and therefore by governor
+        // suspend when wakeup is called
+        when(clock.getSimtime()).thenReturn(42L);
+        when(clock.getRealtime()).thenReturn(0L);
+        when(clock.getRealtime(1000L)).thenReturn(1000L);
+
+        // setup a future task wrapping governor.suspend
+        RunnableFuture<Long> suspender = new FutureTask<Long>(
+                new Callable<Long>() {
+                    @Override
+                    public Long call() throws Exception {
+                        return governor.suspendUntil(1000L);
+                    }
+                });
+        Thread thread = new Thread(suspender);
+        thread.start();
+
+        // wait until suspender got called and governor suspended the thread
+        busyWaitForThreadState(thread, Thread.State.TIMED_WAITING);
+
+        // resume governor. This method is called when an EventSource running
+        // in its own thread wants to get polled.
+        governor.resume(10L);
+
+        // verify the result and the call to clock
+        long result = suspender.get();
+        assertEquals(10L, result);
+
+        verify(clock).getSimtime();
+    }
+
+    @Test
+    public void testSuspendUntilResumeEarlyClockWins() throws Exception {
+        // define a timestamp returned by clock and therefore by governor
+        // suspend when wakeup is called
+        when(clock.getSimtime()).thenReturn(10L);
+        when(clock.getRealtime()).thenReturn(0L);
+        when(clock.getRealtime(1000L)).thenReturn(1000L);
+
+        // setup a future task wrapping governor.suspend
+        RunnableFuture<Long> suspender = new FutureTask<Long>(
+                new Callable<Long>() {
+                    @Override
+                    public Long call() throws Exception {
+                        return governor.suspendUntil(1000L);
+                    }
+                });
+        Thread thread = new Thread(suspender);
+        thread.start();
+
+        // wait until suspender got called and governor suspended the thread
+        busyWaitForThreadState(thread, Thread.State.TIMED_WAITING);
+
+        // resume governor. This method is called when an EventSource running
+        // in its own thread wants to get polled.
+        governor.resume(42L);
+
+        // verify the result and the call to clock
+        long result = suspender.get();
+        assertEquals(10L, result);
+
+        verify(clock).getSimtime();
+    }
+}
