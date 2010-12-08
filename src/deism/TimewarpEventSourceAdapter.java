f9f1142fd6a2bc64e4bff4bc613185ffda6509f8
Lorenz Schori <lo@znerol.ch>
2010-12-08 14:52:15 +0100

    Support antimessages in TimewarpEventSourceAdapter

--- src/deism/TimewarpEventSourceAdapter.java
+++ src/deism/TimewarpEventSourceAdapter.java
@@ -26,21 +26,42 @@ public class TimewarpEventSourceAdapter
     
     @Override
     public Event peek(long currentSimtime) {
-        if (pending.peek() == null) {
-            lastEventFromSource = source.peek(currentSimtime);
+        while (pending.peek() == null && source.peek(currentSimtime) != null) {
+            Event event = null;
+
+            // poll antimessages until we get a normal event or null
+            for (event = source.peek(currentSimtime);
+                    event != null && event.isAntimessage();
+                    event = source.peek(currentSimtime)) {
+                source.remove(event);
+                pending.offer(event);
+            }
+
+            lastEventFromSource = event;
             if (lastEventFromSource != null) {
                 pending.offer(lastEventFromSource);
             }
         }
 
-        return pending.peek();
+        Event result = pending.peek();
+        // An EventSource may not emit antimessages when no corresponding Event
+        // was emitted before.
+        if (result != null && result.isAntimessage()
+                && !containedInHistory(result.inverseEvent())) {
+            result = null;
+        }
+
+        return result;
     }
 
     @Override
     public void remove(Event event) {
         if (event == lastEventFromSource) {
             source.remove(event);
+            lastEventFromSource = null;
         }
+        assert(lastEventFromSource == null);
+
         pending.remove(event);
         pushHistory(event);
     }
