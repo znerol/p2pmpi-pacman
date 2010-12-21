566068b524fc91761d97c6792f50a69a08a6cd65
Lorenz Schori <lo@znerol.ch>
2010-12-21 22:09:39 +0100

    Break dependency between StateHistory and EventRunloopRecoveryStrategy

--- src/deism/run/EventRunloopRecoveryStrategy.java
+++ src/deism/run/EventRunloopRecoveryStrategy.java
@@ -1,6 +1,30 @@
 package deism.run;
 
-import deism.stateful.StateHistory;
+import deism.stateful.StateHistoryException;
 
-public interface EventRunloopRecoveryStrategy extends StateHistory <Long> {
+public interface EventRunloopRecoveryStrategy {
+    /**
+     * Take a snapshot of the current state of the simulation for the given
+     * timestamp and store it for later rollback or commit operations.
+     */
+    public void save(Long timestamp) throws StateHistoryException;
+
+    /**
+     * States up to but not including the one associated with the given
+     * timestamp are not subject to any operation anymore and may be disposed.
+     * Subsequent rollbacks may not refer to a state preceding the given
+     * timestamp.
+     *
+     * If there is no state associated with this timestamp the next older state
+     * must be used.
+     */
+    public void commit(Long timestamp) throws StateHistoryException;
+
+    /**
+     * Restore the state associated with the given timestamp.
+     *
+     * If there is no state associated with this timestamp the next older state
+     * must be used.
+     */
+    public void rollback(Long timestamp) throws StateHistoryException;
 }
