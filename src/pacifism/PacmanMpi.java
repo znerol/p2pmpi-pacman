6edc3fadeedb8945c70500803409184bfac45d76
Lorenz Schori <lo@znerol.ch>
2011-01-16 12:23:51 +0100

    Pacman mpi main class

--- src/pacifism/PacmanMpi.java
+++ src/pacifism/PacmanMpi.java
@@ -1,13 +1,49 @@
 package pacifism;
 
+import org.apache.log4j.Appender;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Layout;
+import org.apache.log4j.PatternLayout;
+
+import p2pmpi.mpi.MPI;
+
 public class PacmanMpi {
+    private static int GVT_TQ_SIZE = 100;
+    private static int GVT_MASTER_RANK = 2;
+    private static int GVT_REPORT_TAG = 1;
+    private static int PAC_EVENT_TAG = 2;
+    private static double TIME_SCALE = 1000 / 60;
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        args = MPI.Init(args);
+        int rank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
 
+        // we currently support two players
+        assert (size == 3);
+
+        // setup log4j, display mpi rank at the beginning of a line
+        Layout layout =
+                new PatternLayout(MPI.COMM_WORLD.Rank() + " "
+                        + PatternLayout.TTCC_CONVERSION_PATTERN);
+        Appender appender = new ConsoleAppender(layout);
+        BasicConfigurator.configure(appender);
+
+        // setup gvt master and game nodes
+        if (rank == GVT_MASTER_RANK) {
+            GvtMasterNode node =
+                    new GvtMasterNode(MPI.COMM_WORLD, GVT_MASTER_RANK,
+                            GVT_REPORT_TAG, size - 1, TIME_SCALE);
+            node.run();
+        }
+        else {
+            GameNode node =
+                    new GameNode(MPI.COMM_WORLD, GVT_MASTER_RANK,
+                            GVT_REPORT_TAG, rank, GVT_TQ_SIZE, TIME_SCALE);
+            node.run();
+        }
+
+        MPI.Finalize();
     }
-
 }
