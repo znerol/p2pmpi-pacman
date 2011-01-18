znerol/p2pmpi-pacman

commit 3cc9c4c2476e0e9a9691c47b52df7b25224cd189

Documentation updates

src/deism/run/Service.java
@@ -13,6 +13,9 @@ import deism.ipc.base.EventImporter;
 import deism.stateful.StateHistory;
 import deism.stateful.StateHistoryException;
 
+/**
+ * Supporting services for the runloop.
+ */
 public class Service implements Startable, StateHistory<Long>, Flushable,
         EventImporter, EventExporter, LvtListener {
 
@@ -40,7 +43,8 @@ public class Service implements Startable, StateHistory<Long>, Flushable,
     private final static Logger logger = Logger.getLogger(Service.class);
 
     /**
-     * Register secondary interfaces of the given object with the process.
+     * Add the given object as an observer to every possible subject known to
+     * the service class
      * 
      * @param object
      */
@@ -48,52 +52,28 @@ public class Service implements Startable, StateHistory<Long>, Flushable,
     public void register(Object object) {
         if (object instanceof Startable) {
             logger.debug("Register startable " + object);
-            addStartable((Startable) object);
+            startableList.add((Startable) object);
         }
         if (object instanceof Flushable) {
             logger.debug("Register flushable " + object);
-            addFlushable((Flushable) object);
+            flushables.add((Flushable) object);
         }
         if (object instanceof StateHistory<?>) {
             logger.debug("Register state aware " + object);
-            addStatefulObject((StateHistory<Long>) object);
+            statefulObjects.add((StateHistory<Long>) object);
         }
         if (object instanceof LvtListener) {
-            logger.debug("Set lvt listener " + object);
-            addLvtListener((LvtListener) object);
+            logger.debug("Register lvt listener " + object);
+            lvtListeners.add((LvtListener) object);
         }
         if (object instanceof EventImporter) {
             logger.debug("Set event importer " + object);
-            setEventImporter((EventImporter) object);
+            this.eventImporter = (EventImporter) eventImporter;
         }
         if (object instanceof EventExporter) {
             logger.debug("Set event exporter " + object);
-            setEventExporter((EventExporter) object);
+            this.eventExporter = (EventExporter) eventExporter;
         }
-    }
-
-    private void addStartable(Startable startable) {
-        startableList.add(startable);
-    }
-
-    private void addStatefulObject(StateHistory<Long> statefulObject) {
-        statefulObjects.add(statefulObject);
-    }
-
-    private void addFlushable(Flushable flushable) {
-        flushables.add(flushable);
-    }
-
-    private void addLvtListener(LvtListener lvtListener) {
-        lvtListeners.add(lvtListener);
-    }
-
-    private void setEventImporter(EventImporter eventImporter) {
-        this.eventImporter = eventImporter;
-    }
-
-    private void setEventExporter(EventExporter eventExporter) {
-        this.eventExporter = eventExporter;
     }
 
     @Override

src/deism/run/LvtListener.java
@@ -1,5 +1,17 @@
 package deism.run;
 
+/**
+ * Registered lvt listeners will get notified whenever the local virtual time
+ * changes.
+ * 
+ * @see {@link Service}
+ */
 public interface LvtListener {
+    /**
+     * Local virtual time changed to the given value in this simulation
+     * 
+     * @param lvt
+     *            new local virtual time
+     */
     public void update(long lvt);
 }

src/deism/run/MessageCenter.java
@@ -13,31 +13,40 @@ import deism.ipc.base.FilteredHandler;
 import deism.ipc.base.Handler;
 import deism.ipc.base.Message;
 
+/**
+ * Part of the runloop where ipc messages are exchanged.
+ */
 public class MessageCenter implements Endpoint<Message> {
 
     private final ExecutionGovernor governor;
     private final Queue<Message> queue = new ArrayDeque<Message>();
     private final List<Handler<Message>> handlers =
-        new ArrayList<Handler<Message>>();
+            new ArrayList<Handler<Message>>();
     private final List<Endpoint<Message>> endpoints =
-        new ArrayList<Endpoint<Message>>();
+            new ArrayList<Endpoint<Message>>();
 
     public MessageCenter(ExecutionGovernor governor) {
         this.governor = governor;
     }
 
-    public synchronized Message poll() {
-        return queue.poll();
-    }
-
-    public void process() {
+    /**
+     * Loop thru all available messages and dispatch them.
+     */
+    public synchronized void process() {
         Message message;
         while ((message = queue.poll()) != null) {
             dispatch(message);
         }
     }
 
-    public void dispatch(Message message) {
+    /**
+     * Deliver the given message to every handler and endpoint known to this
+     * MessageCenter
+     * 
+     * @param message
+     *            Message to dispatch
+     */
+    private void dispatch(Message message) {
         for (Handler<Message> handler : handlers) {
             handler.handle(message);
         }
@@ -46,24 +55,60 @@ public class MessageCenter implements Endpoint<Message> {
         }
     }
 
+    /**
+     * Register the given handler in this message center.
+     * 
+     * @param handler
+     *            message handler
+     */
     public void addHandler(Handler<Message> handler) {
         handlers.add(handler);
     }
 
+    /**
+     * Register the given handler in this message center for all messages
+     * satisfying the given condition.
+     * 
+     * @param handler
+     *            message handler
+     * @param condition
+     *            filter condition for messages
+     */
     public void addHandler(Handler<Message> handler,
             Condition<Message> condition) {
         handlers.add(new FilteredHandler<Message>(handler, condition));
     }
 
+    /**
+     * Register the given endpoint in this message center.
+     * 
+     * @param endpoint
+     *            message endpoint
+     */
     public void addEndpoint(Endpoint<Message> Endpoint) {
         endpoints.add(Endpoint);
     }
 
+    /**
+     * Register the given endpoint in this message center for all messages
+     * satisfying the given condition.
+     * 
+     * @param endpoint
+     *            message endpoint
+     * @param condition
+     *            filter condition for messages
+     */
     public void addEndpoint(Endpoint<Message> endpoint,
             Condition<Message> condition) {
         endpoints.add(new FilteredEndpoint<Message>(endpoint, condition));
     }
 
+    /**
+     * Set the endpoint of the given emitter to this message center.
+     * 
+     * @param emitter
+     *            message emitter
+     */
     public void addEmitter(Emitter<Message> emitter) {
         emitter.setEndpoint(this);
     }

src/deism/run/StateHistoryController.java
@@ -6,6 +6,9 @@ import java.util.TreeSet;
 import deism.stateful.StateHistory;
 import deism.stateful.StateHistoryException;
 
+/**
+ * A {@link StateController} implementation controlling {@link StateHistory}
+ */
 public class StateHistoryController implements StateController {
 
     private SortedSet<Long> snapshots = new TreeSet<Long>();
@@ -53,7 +56,7 @@ public class StateHistoryController implements StateController {
         snapshots.headSet(timestamp).clear();
         if (snapshots.size() == 0) {
             throw new StateHistoryException(
-                    "Attempt to rollback to a timestamp which was never recorded");
+                    "Attempt to commit a timestamp which was never recorded");
         }
 
         Long snapshotKey = snapshots.first();

src/deism/run/SystemTimeProxy.java
@@ -1,5 +1,9 @@
 package deism.run;
 
+/**
+ * Helper class in order to avoid call to non-mockable static functions from
+ * {@link RealtimeExecutionGovernor}.
+ */
 public class SystemTimeProxy {
     public long get() {
         return System.currentTimeMillis();

src/deism/run/Timebase.java
@@ -1,7 +1,8 @@
 package deism.run;
 
 /**
- * Convert between different time bases
+ * Provide scalable and convertible time system. Reference time units are
+ * milliseconds.
  */
 public class Timebase {
     private long base;
@@ -24,34 +25,45 @@ public class Timebase {
         this(0, 1.0);
     }
 
+    /**
+     * Return time scale factor. Reference time units are milliseconds.
+     * 
+     * @return time scale factor
+     */
     public double getScale() {
         return scale;
     }
 
+    /**
+     * Set time scale factor. Reference time units are milliseconds.
+     */
     public void setScale(double scale) {
         this.scale = scale;
     }
 
     /**
-     * Set simulation time baseline to given timestamp
+     * Set start time to given timestamp
      */
     public void setTimebase(long base) {
         this.base = base;
     }
 
     /**
-     * Return time base
+     * Return start time
      */
     public long getTimebase() {
         return base;
     }
 
     /**
-     * Return simulation time represented by the given walltime
+     * Convert timestamp in units of this timebase to a timestamp in units of
+     * the target timebase.
      * 
-     * @param currentTimeMillis
-     *            walltime
-     * @return walltime converted to simtime
+     * @param timestamp
+     *            in units of this timebase
+     * @param target
+     *            other timebase
+     * @return timestamp in units of the target timebase
      */
     public long convert(long timestamp, Timebase target) {
         long dt = (long) (target.scale / this.scale * (timestamp - this.base));

src/deism/run/package-info.java
@@ -1,0 +1,4 @@
+/**
+ * Implementation of the simulation runloop as well as supporting classes
+ */
+package deism.run;

commit c5ee57eb7ee290002e70d0029aaef1d02b46f1be

Bugfix for failing Pingpong example
Fix a bug introduced in changeset:3cc9c4c2 which resulted that
eventImporter and eventExporter was not registered in Service. That lead
to failing GVT algorithm because GVT-Report is triggered by the import /
export mechanism.

src/deism/run/Service.java
@@ -68,11 +68,11 @@ public class Service implements Startable, StateHistory<Long>, Flushable,
         }
         if (object instanceof EventImporter) {
             logger.debug("Set event importer " + object);
-            this.eventImporter = (EventImporter) eventImporter;
+            this.eventImporter = (EventImporter) object;
         }
         if (object instanceof EventExporter) {
             logger.debug("Set event exporter " + object);
-            this.eventExporter = (EventExporter) eventExporter;
+            this.eventExporter = (EventExporter) object;
         }
     }
 