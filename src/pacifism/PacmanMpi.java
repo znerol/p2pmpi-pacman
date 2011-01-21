fe4c095347a0a9dc5baf4b4a727bc5f96893d7e2
Lorenz Schori <lo@znerol.ch>
2011-01-21 13:18:30 +0100

    speed, seed, loglevel tqsize arguments

--- src/pacifism/PacmanMpi.java
+++ src/pacifism/PacmanMpi.java
@@ -9,16 +9,19 @@ import org.apache.log4j.Appender;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.ConsoleAppender;
 import org.apache.log4j.Layout;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 import org.apache.log4j.PatternLayout;
 
 import p2pmpi.mpi.MPI;
 
 public class PacmanMpi {
-    private static int GVT_TQ_SIZE = 100;
+    private static double BASE_TIME_SCALE = 60. / 1000.;
+    private static int DEFAULT_TQ_SIZE = 100;
+
     private static int GVT_MASTER_RANK = 2;
     private static int GVT_REPORT_TAG = 1;
     private static int PAC_EVENT_TAG = 2;
-    private static double TIME_SCALE = 60. / 1000.;
 
     public static void main(String[] args) {
         args = MPI.Init(args);
@@ -34,20 +37,25 @@ public class PacmanMpi {
                         + PatternLayout.TTCC_CONVERSION_PATTERN);
         Appender appender = new ConsoleAppender(layout);
         BasicConfigurator.configure(appender);
-/*
-        if (args.length == 1 && args[0].equals("-d")) {
-            Logger.getRootLogger().setLevel(Level.ALL);
-        }
-        else {
-            Logger.getRootLogger().setLevel(Level.WARN);
-        }
-        */
+
+        /* Game speed parameter */
+        double speed = Double.valueOf(System.getProperty("speed", "1.0"));
+
+        /* Random seed parameter */
+        long seed = Long.valueOf(System.getProperty("seed", "0"));
+
+        /* Log level parameter */
+        Level loglevel = Level.toLevel(System.getProperty("loglevel", "WARN"));
+        Logger.getRootLogger().setLevel(loglevel);
+
+        /* time quantum size */
+        int tqSize = Integer.getInteger("tqsize", DEFAULT_TQ_SIZE);
 
         // setup gvt master and game nodes
         if (rank == GVT_MASTER_RANK) {
             GvtMasterNode node =
                     new GvtMasterNode(MPI.COMM_WORLD, GVT_MASTER_RANK,
-                            GVT_REPORT_TAG, size - 1, TIME_SCALE);
+                            GVT_REPORT_TAG, size - 1, speed);
             node.run();
         }
         else {
@@ -88,18 +96,18 @@ public class PacmanMpi {
             for (Sprite sprite : model.getSprites()) {
                 if (sprite.isGhost())
                     continue;
-                
+
                 if (i == rank) {
                     spriteId = sprite.getSpriteId();
                     break;
                 }
-                i++;                    
+                i++;
             }
-            
+
             GameNode node =
                     new GameNode(MPI.COMM_WORLD, GVT_MASTER_RANK,
-                            GVT_REPORT_TAG, rank, GVT_TQ_SIZE, PAC_EVENT_TAG,
-                            spriteId, TIME_SCALE, model);
+                            GVT_REPORT_TAG, rank, tqSize, PAC_EVENT_TAG,
+                            spriteId, BASE_TIME_SCALE * speed, seed, model);
             node.run();
         }
 
